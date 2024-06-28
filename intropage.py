2301afd90cd98d0956c7b421f5725b205d3fcb32
srlegramante <eriklegramante@gmail.com>
2024-06-28 13:53:34 -0300

    Finalização do Quiz

--- intropage.py
+++ intropage.py
@@ -5,7 +5,7 @@ class IntroApp:
     def __init__(self, root):
         self.root = root
         self.root.geometry("500x500")
-        self.root.title("Animals")
+        self.root.title("Animais")
         self.root.iconbitmap('./img/gamer.ico')
 
         self.frame = customtkinter.CTkFrame(self.root, width=300, height=300, corner_radius=10, fg_color="#FFFFFF")
@@ -14,17 +14,17 @@ class IntroApp:
         self.label_basic = customtkinter.CTkLabel(self.frame, text="Digite seus nomes e vamos começar!", font=("Arial bold", 18), corner_radius=15, text_color="black")
         self.label_basic.pack(pady=10)
 
-        self.name1 = customtkinter.CTkEntry(self.frame, placeholder_text="Digite o 1° nome: ", placeholder_text_color="black", fg_color="#B0C4DE", text_color="black")
-        self.name1.pack(pady=30)
+        self.name1_entry = customtkinter.CTkEntry(self.frame, placeholder_text="Digite o 1° nome: ", placeholder_text_color="black", fg_color="#B0C4DE", text_color="black")
+        self.name1_entry.pack(pady=30)
 
-        self.name2 = customtkinter.CTkEntry(self.frame, placeholder_text="Digite o 2° nome: ", placeholder_text_color="black", fg_color="#B0C4DE", text_color="black")
-        self.name2.pack(pady=50)
+        self.name2_entry = customtkinter.CTkEntry(self.frame, placeholder_text="Digite o 2° nome: ", placeholder_text_color="black", fg_color="#B0C4DE", text_color="black")
+        self.name2_entry.pack(pady=50)
 
-        self.btn_pronto = customtkinter.CTkButton(self.frame, text="Pronto!", border_spacing=5, command=self.event)
-        self.btn_pronto.pack()
+        self.btn_ready = customtkinter.CTkButton(self.frame, text="Pronto!", border_spacing=5, command=self.event)
+        self.btn_ready.pack()
 
-        label_basic = customtkinter.CTkLabel(self.frame, text="")
-        label_basic.pack(pady=10)
+        self.label_basic = customtkinter.CTkLabel(self.frame, text="")
+        self.label_basic.pack(pady=10)
 
         self.error_label = customtkinter.CTkLabel(self.frame, text="", text_color="red")
         self.error_label.pack(pady=10)
@@ -32,30 +32,29 @@ class IntroApp:
         self.names_list = []
 
     def event(self):
-        if self.verify_type_names():
-            self.names()
+        if self.verify_names():
+            self.set_names()
             self.root.destroy()
             mainpage = customtkinter.CTk()
             MainPage(mainpage, self.names_list)
             mainpage.mainloop()
         else:
-            self.error_label.configure(text="Por favor, digite caracteres válidos.")
+            self.error_label.configure(text="Por favor, insira caracteres válidos.")
 
-    def verify_type_names(self):
+    def verify_names(self):
         invalid_chars = set('#!"$%¨&*()-_=]')
-        user1 = self.name1.get()
-        user2 = self.name2.get()
+        user1 = self.name1_entry.get()
+        user2 = self.name2_entry.get()
         
         if any(char in invalid_chars for char in user1) or any(char in invalid_chars for char in user2):
             return False
         return True
 
-    def names(self):
-        user1 = self.name1.get()
-        user2 = self.name2.get()
+    def set_names(self):
+        user1 = self.name1_entry.get()
+        user2 = self.name2_entry.get()
 
         self.names_list = [user1, user2]
-        
 
 if __name__ == "__main__":
     root = customtkinter.CTk()
--- mainpage.py
+++ mainpage.py
@@ -5,19 +5,23 @@ class MainPage:
     def __init__(self, root, names):
         self.root = root
         self.root.geometry("700x500")
-        self.root.title("Animals")
+        self.root.title("Animais")
         self.root.iconbitmap('./img/gamer.ico')
 
+        self.player1_points = 0
+        self.player2_points = 0
+        self.player1_turn = True  # Variável para determinar de quem é a vez
+
         frame1 = customtkinter.CTkFrame(self.root, width=150, height=150, corner_radius=10)
-        frame1.pack(pady= 20, padx= 20)
+        frame1.pack(pady=20, padx=20)
 
         self.label1 = customtkinter.CTkLabel(frame1, text=f"Olá jogadores {' e '.join(names)}")
         self.label1.pack(pady=20, padx=20)
 
-        label2 = customtkinter.CTkLabel(frame1, text="Um animal aleatorio irá aparecer, tente advinha-lo!")
+        label2 = customtkinter.CTkLabel(frame1, text="Um animal aleatório irá aparecer, tente adivinhar!")
         label2.pack(pady=20, padx=20)
 
-        self.btn_ver_dica = customtkinter.CTkButton(frame1, text="Ver dica: ", command=self.run_game_logic)
+        self.btn_ver_dica = customtkinter.CTkButton(frame1, text="Ver dica", command=self.run_game_logic)
         self.btn_ver_dica.pack(pady=20, padx=20)
 
         self.hint_label = customtkinter.CTkLabel(frame1, text="", text_color="yellow", width=200)
@@ -26,17 +30,20 @@ class MainPage:
         self.result_label = customtkinter.CTkLabel(frame1, text="", text_color="green")
         self.result_label.pack(pady=10, padx=10)
 
+        self.sorteio_nomes = customtkinter.CTkLabel(root, text="", text_color="yellow")
+        self.sorteio_nomes.place(x=600, y=10)
+
         self.error_label = customtkinter.CTkLabel(frame1, text="", text_color="red")
         self.error_label.pack(pady=10)
 
-        self.resposta_jogador = customtkinter.CTkEntry(frame1, placeholder_text="Qual é o animal? ", text_color="black", placeholder_text_color="black", fg_color="#B0C4DE")
+        self.resposta_jogador = customtkinter.CTkEntry(frame1, placeholder_text="Qual é o animal?", text_color="black", placeholder_text_color="black", fg_color="#B0C4DE")
         self.resposta_jogador.pack(pady=20, padx=20)
 
         self.btn_conferir = customtkinter.CTkButton(frame1, text="Conferir", command=self.conferir_resposta, width=75, height=30, text_color="black")
         self.btn_conferir.pack()
 
-        self.pontos = customtkinter.CTkLabel(frame1, text="")
-        self.pontos.pack(pady=20)
+        self.pontos = customtkinter.CTkLabel(root, text="")
+        self.pontos.place(x=10, y=10)
 
     def lista_animais(self):
         return {
@@ -48,18 +55,16 @@ class MainPage:
             "Papagaio": "O animal tem capacidade de imitar sons humanos e é conhecido por sua coloração vibrante.",
             "Ornitorrinco": "O animal é um mamífero que põe ovos e tem um bico semelhante ao de um pato.",
             "Abelha": "O animal é um inseto que produz mel, tem um ferrão e vive em colônias.",
-            "Dragão de comodo": "O animal é uma espécie de lagarto, é venenoso e é conhecido por sua língua bifurcada.",
             "Panda": "É preto e branco, se alimenta de bambu e vive nas florestas da China.",
             "Tigre": "Tem listras, é um excelente caçador e vive em florestas e savanas.",
             "Golfinho": "Vive no mar, é inteligente e se comunica através de sons.",
             "Girafa": "Tem pescoço longo, come folhas de árvores altas e vive nas savanas africanas.",
             "Urso Polar": "Vive no Ártico, tem uma camada de gordura para se proteger do frio e é um excelente nadador.",
-            "Rinoceronte": "Tem um chifre na testa, é herbívoro e vive em savanas e floresself.tas.",
+            "Rinoceronte": "Tem um chifre na testa, é herbívoro e vive em savanas e florestas.",
             "Lobo": "Vive em matilhas, é um excelente caçador e se comunica através de uivos.",
             "Pinguim": "Vive na Antártida, é ótimo nadador e se desloca deslizando sobre o gelo.",
             "Macaco": "Vive em árvores, é ágil e tem habilidades para se balançar de galho em galho.",
             "Leopardo": "Tem manchas rosetas, é um excelente escalador e vive em diversas regiões da África e Ásia.",
-            "Elefante Marinho": "Vive no oceano, é um excelente mergulhador e tem uma probóscide.",
             "Canguru": "Tem patas traseiras fortes, vive na Austrália e é capaz de saltar longas distâncias.",
             "Cobra": "É um réptil, tem uma língua bifurcada e é um predador.",
             "Polvo": "Vive no mar, tem tentáculos e se camufla para se proteger de predadores.",
@@ -70,7 +75,6 @@ class MainPage:
         animais = self.lista_animais()
         animal, dica = random.choice(list(animais.items()))
         return animal, dica
-    
 
     def run_game_logic(self):
         self.btn_ver_dica.configure(state=customtkinter.DISABLED)
@@ -78,23 +82,46 @@ class MainPage:
         self.hint_label.configure(text=self.dica)
         print(f"O animal escolhido foi: {self.animal}")
 
-        resposta = self.resposta_jogador.get()
-
-        if resposta == self.animal:
-            self.root.after(5000, self.limpar_result)
+        if self.player1_turn:
+            self.result_label.configure(text="É a vez do jogador 1!")
         else:
-            pass
+            self.result_label.configure(text="É a vez do jogador 2!")
 
     def conferir_resposta(self):
         resposta = self.resposta_jogador.get()
 
-        if resposta == self.animal:
-            self.result_label.configure(text="Parabéns! Você acertou, vamos para o proximo!")
-            self.btn_ver_dica.configure(state=customtkinter.NORMAL)
-            self.root.after(4000, self.limpar_result)
+        if self.player1_turn:
+            if resposta.lower() == self.animal.lower():
+                self.result_label.configure(text_color="green")
+                self.result_label.configure(text="Parabéns! Jogador 1 acertou, vamos para o próximo!")
+                self.player1_points += 1
+            else:
+                self.player1_turn = False
+                self.result_label.configure(text_color="red")
+                self.result_label.configure(text="Ops... Jogador 1 errou, é a vez do jogador 2")
         else:
-            self.result_label.configure(text_color="red")
-            self.result_label.configure(text="Ops... você errou, é a vez do jogador: ")
+            if resposta.lower() == self.animal.lower():
+                self.result_label.configure(text_color="green")
+                self.result_label.configure(text="Parabéns! Jogador 2 acertou, vamos para o próximo!")
+                self.player2_points += 1
+            else:
+                self.player1_turn = True
+                self.result_label.configure(text_color="red")
+                self.result_label.configure(text="Ops... Jogador 2 errou, é a vez do jogador 1")
+
+        self.pontos.configure(text=f"Jogador 1:   {self.player1_points} pontos\nJogador 2:   {self.player2_points} pontos")
+        self.root.after(4000, self.limpar_result)
+
+        if self.player1_points >= 10:
+            self.result_label.configure(text="Jogador 1 venceu!")
+            self.btn_ver_dica.configure(state=customtkinter.DISABLED)
+            self.btn_conferir.configure(state=customtkinter.DISABLED)
+        elif self.player2_points >= 10:
+            self.result_label.configure(text="Jogador 2 venceu!")
+            self.btn_ver_dica.configure(state=customtkinter.DISABLED)
+            self.btn_conferir.configure(state=customtkinter.DISABLED)
+        else:
+            self.btn_ver_dica.configure(state=customtkinter.NORMAL)
 
     def limpar_result(self):
         self.result_label.configure(text="")
@@ -109,5 +136,3 @@ class MainPage:
         if any(char in invalid_chars for char in answer):
             return False
         return True
-        
-            
